f45d82db2174822da83c3c636132a32410f78cc1
Saadi Save <saadisave@gmail.com>
2021-01-07 23:17:32 +0530

    CLI complete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,50 @@
 mod vex;
 
+#[macro_use]
+extern crate clap;
+use clap::App;
+use vex::Vextract;
+
 fn main() {
+    let conf = load_yaml!("cli.yml");
+    let matches = App::from_yaml(conf).get_matches();
+
+    let infile = matches.value_of("infile").unwrap();
+    let mut outfile: Option<&str> = None;
+    let pr = matches.is_present("print");
+    let mut alist: Vec<&str> = Vec::new();
+    let mut plist: Vec<&str> = Vec::new();
+
+    match matches.value_of("outfile") {
+        Some(s) => outfile = Some(s),
+        None => ()
+    }
+
+    match matches.value_of("alist") {
+        Some(s) => {
+            for i in s.split(' ') {
+                alist.push(i.clone());
+            }
+        },
+        None => ()
+    }
+
+    match matches.value_of("plist") {
+        Some(s) => {
+            for i in s.split(' ') {
+                plist.push(i.clone());
+            }
+        },
+        None => ()
+    }
+
+    let out = Vextract::new(infile, alist, plist);
+    if pr == true {
+        println!("{}", out.get_sorted_pretty_vocab())
+    }
     
+    match outfile {
+        Some(s) => out.write_to_file(s),
+        None => ()
+    }
 }
